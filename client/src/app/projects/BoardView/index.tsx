9d41e3b4f0564bf9dc5c04b87e60b744c568597b
Joshua Levinson <124539621+jlevinson-intermedia@users.noreply.github.com>
2024-10-13 17:48:36 +1100

    fix redux type errors. build for production.

--- client/src/app/projects/BoardView/index.tsx
+++ client/src/app/projects/BoardView/index.tsx
@@ -2,6 +2,7 @@ import { useGetTasksQuery, useUpdateTaskStatusMutation } from '@/state/api';
 import React from 'react'
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DropTargetMonitor, DragSourceMonitor } from 'react-dnd';
 import { Task as TaskType } from '@/state/api';
 import { EllipsisVertical, MessageSquareMore, Plus } from 'lucide-react';
 import { format } from 'date-fns';
@@ -62,7 +63,7 @@ const TaskColumn = ({
    const [{ isOver }, drop ] = useDrop(() => ({
     accept: "task",
     drop: (item: { id: number }) => moveTask(item.id, status),
-    collect: (monitor: any) => ({
+    collect: (monitor: DropTargetMonitor) => ({
         isOver: !!monitor.isOver()
     })
    }));
@@ -127,7 +128,7 @@ const Task = ({ task }: TaskProps) => {
     const [{ isDragging }, drag ] = useDrag(() => ({
         type: "task",
         item: { id: task.id },
-        collect: (monitor: any) => ({
+        collect: (monitor: DragSourceMonitor) => ({
             isDragging: !!monitor.isDragging()
         })
        }));
--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -1,5 +1,4 @@
-import { useRef } from "react";
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore} from "@reduxjs/toolkit";
 import {
   TypedUseSelectorHook,
   useDispatch,
@@ -10,68 +9,27 @@ import globalReducer from "@/state";
 import { api } from "@/state/api";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import { PersistGate } from "redux-persist/integration/react";
-import createWebStorage from "redux-persist/lib/storage/createWebStorage";
-
-/* REDUX PERSISTENCE */
-const createNoopStorage = () => {
-  return {
-    getItem(_key: any) {
-      return Promise.resolve(null);
-    },
-    setItem(_key: any, value: any) {
-      return Promise.resolve(value);
-    },
-    removeItem(_key: any) {
-      return Promise.resolve();
-    },
-  };
-};
-
-const storage =
-  typeof window === "undefined"
-    ? createNoopStorage()
-    : createWebStorage("local");
-
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["global"],
-};
+/* ROOT REDUCER */
 const rootReducer = combineReducers({
   global: globalReducer,
   [api.reducerPath]: api.reducer,
 });
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 /* REDUX STORE */
-export const makeStore = () => {
-  return configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefault) =>
-      getDefault({
-        serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-      }).concat(api.middleware),
-  });
-};
+export const store = configureStore({
+  reducer: rootReducer,
+  // @ts-expect-error: find a way to prevent middleware from having a type error
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+});
+
+// Setup listeners for RTK Query
+setupListeners(store.dispatch);
 
 /* REDUX TYPES */
-export type AppStore = ReturnType<typeof makeStore>;
-export type RootState = ReturnType<AppStore["getState"]>;
-export type AppDispatch = AppStore["dispatch"];
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 /* PROVIDER */
@@ -80,18 +38,5 @@ export default function StoreProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore>();
-  if (!storeRef.current) {
-    storeRef.current = makeStore();
-    setupListeners(storeRef.current.dispatch);
-  }
-  const persistor = persistStore(storeRef.current);
-
-  return (
-    <Provider store={storeRef.current}>
-      <PersistGate loading={null} persistor={persistor}>
-        {children}
-      </PersistGate>
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 }
