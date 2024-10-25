417e1b5cfb123f7035ff7b0d5db9a6e5ddb090e8
Joshua Levinson <124539621+jlevinson-intermedia@users.noreply.github.com>
2024-10-26 06:56:27 +1100

    upgrade packages in client/server - major update (Next 15). Implement base auth provider ui using Amplify

---
+++ client/src/app/authProvider.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure({
+    Auth: {
+        Cognito: {
+            userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || "",
+            userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID || ""
+        }
+    }
+})
+
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="mt-5">
+        <Authenticator>
+        {({user}) => user ? (
+            <div>{children}</div>
+        ) : (
+            <div>
+                <h1>Please sign in below:</h1>
+            </div>
+        )}
+        </Authenticator>
+    </div>
+    )
+}
+
+export default AuthProvider
