7ab74f22cc3e94cfbdf97a43939f4cf49db6a7b8
Joshua Levinson <124539621+jlevinson-intermedia@users.noreply.github.com>
2024-10-27 22:06:35 +1100

    update config mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -10,13 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-    NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID:
-      process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID,
-    NEXT_PUBLIC_COGNITO_USER_POOL_ID:
-      process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID,
-  },
 };
 
 export default nextConfig;
