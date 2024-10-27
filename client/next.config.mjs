19bf8ecb41553aad709fea45eb8b53caf59e3cb1
Joshua Levinson <124539621+jlevinson-intermedia@users.noreply.github.com>
2024-10-27 21:55:55 +1100

    build: setup Amplify environment variables

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -10,6 +10,13 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID:
+      process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID,
+    NEXT_PUBLIC_COGNITO_USER_POOL_ID:
+      process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID,
+  },
 };
 
 export default nextConfig;
