df87b43569ac60b53e3cf32f7995791651938844
Joshua Levinson <124539621+jlevinson-intermedia@users.noreply.github.com>
2024-10-27 23:51:09 +1100

    remove hardcoded settings page for user and email

--- client/src/app/settings/page.tsx
+++ client/src/app/settings/page.tsx
@@ -3,14 +3,18 @@
 import Header from '@/components/Header';
 import React from 'react'
 
+import { useGetAuthUserQuery } from "@/state/api";
 
 const Settings = () => {
+    const { data: currentUser } = useGetAuthUserQuery({});
+
     const userSettings = {
-        username: "johndoe",
-        email: "johndoe@gmail.com",
+        username: currentUser?.userDetails.username || "",
+        email: currentUser?.userDetails.email || "",
         teamName: "Development Team",
         roleName: "Developer",
     }
+
 
     const labelStyles = "block text-sm font-medium dark:text-white";
     const textStyles = "mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 dark:text-white";
